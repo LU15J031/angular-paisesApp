f2b273bcf4c8c6ebe5c966b5a072cd49011ab0dc
Luis Joel <luisjoelcastro1996@gmail.com>
2024-02-20 22:29:53 -0600

    cuarto commit aplicacion de paises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule} from '@angular/common/http'
+
 
 import { AppComponent } from './app.component';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    PaisModule,
+    AppRoutingModule,
+    HttpClientModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
